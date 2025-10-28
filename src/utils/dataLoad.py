b32a8332633fd47597acf34e1989441fed67df49
dylan <dylan@DESKTOP-NM1RGCO.attlocal.net>
2025-10-28 15:54:35 -0500

    loading ember ok

--- src/utils/dataLoad.py
+++ src/utils/dataLoad.py
@@ -1,23 +1,37 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
+from pyarrow.parquet import ParquetFile
+import pyarrow as pa
 
-def loadEmber(dataPath="/home/dylan/Documents/Adversarial-Security-Project/Data/Ember",data='Train'):
+def loadEmber(dataPath="/home/dylan/Documents/Adversarial-Security-Project/Data/Ember",sampleCount=250000): #no ram need subsamp, data has no struct so can take whatever
     print('Loading Ember...')
+    print('Currently datapath is hardcoded, remember to change that')
 
-    trainDat = pd.read_parquet(f'{dataPath}/train_ember_2018_v2_features.parquet')
-    testDat = pd.read_parquet(f'{dataPath}/test_ember_2018_v2_features.parquet')
+    yTrain = np.squeeze(pd.read_parquet(f'{dataPath}/train_ember_2018_v2_features.parquet',columns=['Label'])
+                        .values.astype('int32'))
 
-    labelCol = trainDat.columns[-1]
-
-    yTrain = trainDat[labelCol].astype('int32')
-    yTest = testDat[labelCol].values.astype('int32')
-    xTrain = trainDat.drop(columns=[labelCol]).values
-    xTest = testDat.drop(columns=[labelCol]).values
+    pf = ParquetFile(f'{dataPath}/train_ember_2018_v2_features.parquet')
+    subSet = next(pf.iter_batches(batch_size=sampleCount))
+    xTrain = pa.Table.from_batches([subSet]).to_pandas()
+    xTrain = xTrain.drop(columns=['Label']).values
+    yTrain = yTrain[:sampleCount]
 
     goodTrain = yTrain!=-1
+
     yTrain = yTrain[goodTrain]
     xTrain = xTrain[goodTrain]
 
+    yTest = np.squeeze(pd.read_parquet(f'{dataPath}/test_ember_2018_v2_features.parquet',columns=['Label'])
+                        .values.astype('int32'))
+    pf = ParquetFile(f'{dataPath}/test_ember_2018_v2_features.parquet')
+    subSet = next(pf.iter_batches(batch_size=int(sampleCount/4)))
+    xTest = pa.Table.from_batches([subSet]).to_pandas()
+    xTest = xTest.drop(columns=['Label']).values
+    yTest = yTrain[:int(sampleCount/4)]
+
     scaler = StandardScaler()
-    scaler.fit_transform(xTrain)
+    xTrain = scaler.fit_transform(xTrain)
+    xTest = scaler.transform(xTest)
+
+    return xTrain, yTrain, xTest, yTest
